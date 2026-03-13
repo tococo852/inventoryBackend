639c4cf3ba3eaf86cc892625e0f9d67c5e97167d
tococo852 <tococo852@gmail.com>
2026-03-13 18:46:56 -0300

    added queries for items table

--- db/db-starter.js
+++ db/db-starter.js
@@ -3,14 +3,14 @@ const { Client } = require("pg");
 const SQL = `
 CREATE TABLE IF NOT EXISTS categories(
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    category_name VARCHAR (255)
+    name VARCHAR (255)
 );
 
 CREATE TABLE IF NOT EXISTS items (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  item_name VARCHAR ( 255 ),
+  name VARCHAR ( 255 ),
   category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
-  item_barcode VARCHAR ( 255 )
+  barcode VARCHAR ( 255 )
 );
 `;
 
--- db/queries.js
+++ db/queries.js
@@ -2,11 +2,29 @@ const pool = require('./pool')
 
 
 const items={
-    async getAll(){},
-    async updateBarcode(item_id, barcode){},
-    async updateName(item_id, name){},
-    async updateCategory(item_id,category_id){},
-    async delete(item_id){}
+    async getAll(){
+        const {rows}= await pool.query('SELECT * FROM items')
+        return rows
+    },
+
+    async add(name, category_id, barcode){
+        await pool.query('INSERT INTO items (name, category_id, barcode) VALUES ( $1,$2,$3)',[name,category_id,barcode] )
+    }
+    ,
+    async updateBarcode(item_id, barcode){
+        await pool.query('UPDATE items SET barcode = $1 where id = $2',[barcode, item_id])
+    },
+    async updateName(item_id, name){
+        await pool.query ('UPDATE items SET name = $1 where id = $2',[name, item_id])
+    },
+    async updateCategory(item_id,category_id){
+         await pool.query ('UPDATE items SET category_id = $1 where id = $2', [category_id, item_id])
+    },
+    async delete(item_id){
+        await pool.query('DELETE FROM items WHERE id=$1', [item_id])
+
+
+    }
 }
 
 
