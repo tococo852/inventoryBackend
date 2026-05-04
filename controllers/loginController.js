29ce4a99f99641f6888dbd4bd7704004e1297611
Tococo852 <tococo852@gmail.com>
2026-05-04 12:07:39 -0400

    removed console logs

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,7 +6,6 @@ const loginController ={
      async login(req,res){
         const data= await users.getPassword(req.body.username)
         const {password} = data[0]
-        console.log(password)
         if (!password) return res.status(401).json({message: 'username or password incorrect'})
         const  match = await bcrypt.compare(req.body.password, password)
         if(match){
