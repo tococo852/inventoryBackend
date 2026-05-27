2e4348ab9dcc700ba1b6693c730efde55e37b9c4
Tococo852 <tococo852@gmail.com>
2026-05-27 13:29:51 -0400

    minor changes

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,5 @@
 const  {Pool} = require('pg')
 
 module.exports= new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+    connectionString: process.env.DB_CONECTION_STRING
 })
