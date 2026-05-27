51776f13e17884e39ac2964b39419f8fa191a57e
Tococo852 <tococo852@gmail.com>
2026-05-27 10:58:58 -0400

    added limiter and proxy trust

--- app.js
+++ app.js
@@ -11,15 +11,22 @@ const { error } = require('node:console')
 const passport = require("passport");
 const  usersRouter  = require('./routers/usersRouter')
 const LocalStrategy = require('passport-local').Strategy;
-
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 500, // max requests per IP
+  message: 'Too many requests, please try again later.',
+})
+app.set('trust proxy', 1)
 app.use(express.json())
-
 app.use(cors())
-
-
+app.use(limiter)
 
 //app.use('/user',usersRouter)
-app.use('/login', loginRouter)
+app.use('/login',rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 15,
+}), loginRouter)
 app.use('/items',itemsRouter)
 app.use('/categories',categoriesRouter)
 app.use('/measures', measuresRouter)
